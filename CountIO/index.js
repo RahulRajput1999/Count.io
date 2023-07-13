088e4207a56595838df72832c30a5e2576df0ea8
Rahul <rahulkumarsindhav@gmail.com>
2023-07-13 19:21:03 +0530

    first exp

---
+++ CountIO/index.js
@@ -0,0 +1,22 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+import { PaperProvider } from 'react-native-paper';
+import { Provider as StoreProvider } from 'react-redux';
+import store from './store';
+
+export default function Main() {
+  return (
+    <StoreProvider store={store}>
+      <PaperProvider>
+        <App />
+      </PaperProvider>
+    </StoreProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
