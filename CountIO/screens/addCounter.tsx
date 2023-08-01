67821125804716ba1a1b2d426e6c26d7592f6b9e
Rahul <rahulkumarsindhav@gmail.com>
2023-08-01 21:27:11 +0530

    simple validation is done

--- CountIO/screens/addCounter.tsx
+++ CountIO/screens/addCounter.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
-import { Appbar, Button, Text, TextInput } from "react-native-paper";
+import { SafeAreaView, StyleSheet, ToastAndroid } from "react-native";
+import { Appbar, Button, TextInput } from "react-native-paper";
 import { connect } from "react-redux";
 import { addCounter } from "../actions/counts";
 
@@ -9,52 +9,61 @@ type Props = {};
 
 class AddCounterScreen extends Component<Props> {
 
+
     addCounter(counter: {}) {
         //need to add data validation here
-        counter.id = this.props.counters.length + 1;
-        this.props.addCounter(counter);
-        this.props.navigation.goBack();
+        if (counter.name === undefined) {
+            ToastAndroid.show("Counter name is required", ToastAndroid.SHORT)
+        }
+        if (counter.desc === undefined) {
+            ToastAndroid.show("Counter description is required", ToastAndroid.SHORT)
+        }
+        if (counter.name && counter.desc) {
+            counter.id = this.props.counters.length + 1;
+            this.props.addCounter(counter);
+            this.props.navigation.goBack();
+        }
+
     }
     render(): React.ReactNode {
-        console.log(this.props)
+
 
         var counter = {
             id: 0,
             count: 0
         }
+
         return (
             <SafeAreaView style={style.safeAreaView}>
                 <Appbar.Header>
                     <Appbar.BackAction onPress={() => { this.props.navigation.goBack() }} />
                     <Appbar.Content title="Add a counter" />
                 </Appbar.Header>
-                <ScrollView style={style.scrollView}>
-                    <TextInput
-                        mode="outlined"
-                        label="Counter Name"
-                        value={counter.name}
-                        onChangeText={text => counter.name = text}
-                        textColor='#ffffff'
-                        outlineColor="#ffffff"
-                        activeOutlineColor="#ffffff"
-                        placeholderTextColor="#ffffff"
-                        style={style.textInput}
-                    />
-                    <TextInput
-                        mode="outlined"
-                        label="Counter Description"
-                        value={counter.desc}
-                        onChangeText={text => counter.desc = text}
-                        textColor='#ffffff'
-                        outlineColor="#ffffff"
-                        activeOutlineColor="#ffffff"
-                        placeholderTextColor="#ffffff"
-                        style={style.textInput}
-                    />
-                    <Button style={style.buttonStyle} mode="contained" onPress={() => this.addCounter(counter)}>
-                        Add Counter
-                    </Button>
-                </ScrollView>
+                <TextInput
+                    mode="outlined"
+                    label="Counter Name"
+                    value={counter.name}
+                    onChangeText={text => counter.name = text}
+                    textColor='#ffffff'
+                    outlineColor="#ffffff"
+                    activeOutlineColor="#ffffff"
+                    placeholderTextColor="#ffffff"
+                    style={style.textInput}
+                />
+                <TextInput
+                    mode="outlined"
+                    label="Counter Description"
+                    value={counter.desc}
+                    onChangeText={text => counter.desc = text}
+                    textColor='#ffffff'
+                    outlineColor="#ffffff"
+                    activeOutlineColor="#ffffff"
+                    placeholderTextColor="#ffffff"
+                    style={style.textInput}
+                />
+                <Button style={style.buttonStyle} mode="contained" onPress={() => this.addCounter(counter)}>
+                    Add Counter
+                </Button>
             </SafeAreaView >
         )
     }
@@ -74,7 +83,7 @@ const style = StyleSheet.create({
     },
     textInput: {
         backgroundColor: '#6852A2',
-        marginTop: 10,
+        margin: 10,
     },
     buttonStyle: {
         marginTop: 10,
